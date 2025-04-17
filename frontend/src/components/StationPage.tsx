c2c35bf0b81e605a6af81867147fd0de1c6e3bb9
Cameron Tacklind <cameron@tacklind.com>
2025-04-17 01:31:26 -0700

    Show radio status on station page

--- frontend/src/components/StationPage.tsx
+++ frontend/src/components/StationPage.tsx
@@ -1,6 +1,12 @@
 import { StationName } from '../../../src/types';
 import StationStatus from './StationStatus';
+import { SystemInfo } from './SystemInfo';
 
 export function StationPage({ station }: { station: StationName }) {
-  return <StationStatus full station={station} />;
+  return (
+    <>
+      <StationStatus full station={station} />
+      <SystemInfo />
+    </>
+  );
 }
